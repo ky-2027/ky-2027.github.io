402ef17ee9bb626a1cbbbc97c7e1f2430fe07655
ky-2027 <kelly.a.avalos1@gmail.com>
2025-11-02 00:17:10 -0400

    Update script.js
    Test 1 - initial js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Optional — smooth scroll for nav links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(e.target.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
 
